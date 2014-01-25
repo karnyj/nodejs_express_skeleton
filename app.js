28fd72972efaf270cd18888bcbfc03c236eb1c0e
visbtc <vis.btc@gmail.com>
2014-01-25 20:17:44 +0200

    removed singly

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-// USAGE:
-//
-// If you have foreman (you should!) set you .env file with
-// SINGLY_CLIENT_ID and SINGLY_CLIENT_SECRET and then run:
-//
-// $ foreman start
-//
-// Otherwise set SINGLY_CLIENT_ID and SINGLY_CLIENT_SECRET and run:
-//
-// $ node app
-
 var express = require('express');
+var mongoStore = require('connect-mongo')(express);
 var querystring = require('querystring');
 var request = require('request');
 var sprintf = require('sprintf').sprintf;
@@ -18,22 +8,16 @@ var partials = require('express-partials');
 // The port that this express app will listen on
 var port = process.env.PORT || 7464;
 
-// Your client ID and secret from http://dev.singly.com/apps
-var clientId = process.env.SINGLY_CLIENT_ID;
-var clientSecret = process.env.SINGLY_CLIENT_SECRET;
+
 
 var hostBaseUrl = (process.env.HOST || 'http://localhost:' + port);
-var apiBaseUrl = process.env.SINGLY_API_HOST || 'https://api.singly.com';
 
 // Create an HTTP server
 var app = express();
 
-// Require and initialize the singly module
-var expressSingly = require('express-singly')(app, clientId, clientSecret,
-  hostBaseUrl, hostBaseUrl + '/callback');
 
 // Pick a secret to secure your session storage
-var sessionSecret = '42';
+var sessionSecret = 'peekaboo';
 
 // Setup for the express web framework
 app.configure(function() {
@@ -45,13 +29,13 @@ app.configure(function() {
   app.use(express.bodyParser());
   app.use(express.cookieParser());
   app.use(express.session({
+    store: new mongoStore({
+        url: 'mongodb://localhost/test-session'
+    }),
     secret: sessionSecret
   }));
-  expressSingly.configuration();
   app.use(app.router);
 });
-
-expressSingly.routes();
 
 // We want exceptions and stracktraces in development
 app.configure('development', function() {
@@ -70,5 +54,4 @@ app.get('/', function(req, res) {
 
 app.listen(port);
 
-console.log(sprintf('Listening at %s using API endpoint %s.', hostBaseUrl,
-  apiBaseUrl));
+console.log(sprintf('Listening at %s using API endpoint %s.', hostBaseUrl));
